9f0d6ae13c72667af2843cb14ecfd01e2ccc68aa
Stefano Chiodino <stefano@Stefanos-MacBook.local>
2017-03-30 22:52:12 +0100

    retrieve at specific time

--- src/Storage.rs
+++ src/Storage.rs
@@ -2,6 +2,7 @@ use data::Data;
 use std::collections::HashMap;
 use chrono::offset::local::Local;
 use chrono::prelude::DateTime;
+use chrono::Duration;
 
 pub struct Storage<T> {
     datum: HashMap<String, Vec<Data<T>>>,
@@ -13,10 +14,10 @@ impl<T> Storage<T> {
     }
 
     pub fn store(&mut self, key: String, value: T) {
-        self.store_at_time(key, value, Local::now());
+        self.store_at_date_time(key, value, Local::now());
     }
 
-    pub fn store_at_time(&mut self, key: String, value: T, date_time: DateTime<Local>) {
+    pub fn store_at_date_time(&mut self, key: String, value: T, date_time: DateTime<Local>) {
         let mut vector = self.datum.entry(key).or_insert(Vec::new());
 
         let data = Data {
@@ -31,6 +32,25 @@ impl<T> Storage<T> {
         let vector = self.datum.get(&key);
         match vector {
             Some(v) => v.last().map(|ref mut x| &x.value),
+            None => None,
+        }
+    }
+
+    pub fn retrieve_at_date_time(&self, key: String, date_time: DateTime<Local>) -> Option<&T> {
+
+        let vector = self.datum.get(&key);
+        match vector {
+            Some(v) => {
+                for (i, data) in v.iter().enumerate() {
+                    if data.date_time == date_time {
+                        return Some(&data.value);
+                    }
+                    if data.date_time > date_time {
+                        return v.get(i - 1).map(|ref mut x| &x.value);
+                    }
+                }
+                None
+            }
             None => None,
         }
     }
@@ -77,8 +97,18 @@ fn retrieve_on_no_data_returns_none() {
 #[test]
 fn can_store_at_specific_time() {
     let mut storage = Storage::new();
-    storage.store_at_time("key".to_string(), 1, Local::now());
+    storage.store_at_date_time("key".to_string(), 1, Local::now());
 }
 
 #[test]
-fn can_retrieve_at_specific_time() {}
+fn can_retrieve_at_specific_time() {
+    let mut storage = Storage::new();
+    let first_date_time = Local::now();
+    let second_date_time = first_date_time.clone() + Duration::hours(1);
+    storage.store_at_date_time("key".to_string(), 1, first_date_time);
+    storage.store_at_date_time("key".to_string(), 2, second_date_time);
+
+    let result = storage.retrieve_at_date_time("key".to_string(), first_date_time);
+
+    assert_eq!(*result.unwrap(), 1);
+}
